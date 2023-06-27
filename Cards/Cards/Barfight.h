4124193b44536a31397cb0022a8d6918374f54b4
yuvyt <130463664+yuvyt@users.noreply.github.com>
2023-06-27 15:20:14 +0300

    Update Barfight.h with descriptions and dtor

--- Cards/Cards/Barfight.h
+++ Cards/Cards/Barfight.h
@@ -5,6 +5,7 @@
 
 #include "Card.h"
 #include "../utilities.h"
+const HEALTH_POINTS_ON_LOSS = 10;
 
 class Barfight : public Card
 {
@@ -12,9 +13,16 @@ private:
     int m_healthPointsLoss;
 
 public:
+	
+    /* c'tor for barfight, initializaing to the default const values*/
     Barfight();
+    /*overloads the applyEncounter function in Card and
+    implements a new version for it specifically when encountering 
+    barfight card*/
     void applyEncounter(Player& player) const override;
+    /*cloning a barfight card*/
     Card *clone() const override;
+    ~Barfight() = default;
 
 
 };
