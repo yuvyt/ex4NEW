e575f5973bc7968448c4cd41040ff7a671892259
yuvyt <130463664+yuvyt@users.noreply.github.com>
2023-06-28 17:57:40 +0300

    Update Dragon.h

--- Cards/Cards/Dragon.h
+++ Cards/Cards/Dragon.h
@@ -15,11 +15,13 @@ class Dragon: public BattleCard
         Dragon();
         /*overloads the applyEncounter function in Card and
         implements a new version for it specifically when encountering 
-        Dragon card*/
+        Dragon card
+        @param: player: player to apply the encounter on*/
         void applyEncounter(Player& player) const override;
         /*overloads the printCard function in Card and
         implements a new version for it specifically when printing 
-        Dragon card*/
+        Dragon card
+        @param: os: ostream to print to*/
         void printCard(std::ostream& os) const override;
         /*defining the d'tpr as default */
         ~Dragon() = default;
