498edc130f19b1dead0fc505ec26d4f53aa275a5
yuvyt <130463664+yuvyt@users.noreply.github.com>
2023-06-28 16:32:16 +0300

    Update Mana.h

--- Cards/Cards/Mana.h
+++ Cards/Cards/Mana.h
@@ -1,20 +1,26 @@
 #ifndef MANA_H
 #define MANA_H
 
-
 #include "Card.h"
 #include "../utilities.h"
+
+const int HEALTH_POINTS_TO_ADD = 10;
 
 class Mana : public Card {
 private:
     int m_healthPoints;
 
 public:
+    /* c'tor for Mana, initializaing to the default const values*/
     Mana();
-
+    /*overloads the applyEncounter function in Card and
+    implements a new version for it specifically when encountering 
+    Mana card*/
     void applyEncounter(Player &player) const override;
-
+    /*cloning a mana card*/
     Card *clone() const override;
+    /*defining the d'tpr as default */
+    ~Mana() = default;
 };
 
 #endif
