2a99832364cf557a9a5d7fc2bb449fe4192a0c8b
yuvyt <130463664+yuvyt@users.noreply.github.com>
2023-06-28 17:15:53 +0300

    Update Well.h

--- Cards/Cards/Well.h
+++ Cards/Cards/Well.h
@@ -5,15 +5,24 @@
 #include "Card.h"
 #include "../utilities.h"
 
+const int HEALTH_POINTS_ON_LOSS = 10;
+
 class Well : public Card
 {
 private:
     int m_healthPointsLoss;
 
 public:
+    /* c'tor for Well, initializaing to the default const values*/
     Well();
+    /*overloads the applyEncounter function in Card and
+    implements a new version for it specifically when encountering 
+    well card*/
     void applyEncounter(Player& player) const override;
+    /*cloning a well card*/
     Card* clone() const override;
+    /*defining the d'tpr as default */
+    ~Well() = default;
 
 };
 
