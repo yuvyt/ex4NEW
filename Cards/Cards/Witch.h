0c2ea5fd6bc32a54e057892d9a758f8e46e0bce5
yuvyt <130463664+yuvyt@users.noreply.github.com>
2023-06-28 17:20:29 +0300

    Update Witch.h

--- Cards/Cards/Witch.h
+++ Cards/Cards/Witch.h
@@ -5,16 +5,30 @@
 
 #include "BattleCard.h"
 
+const int HEALTH_POINTS_ON_LOSS = 10; 
+const int FORCE_POINTS_ON_LOSS = 1; 
+const int CARD_FORCE = 11;
+const int LOOT = 2;
+
 class Witch: public BattleCard
 {
 private:
     int m_powerLoss;
 public:
+    /* c'tor for Witch, initializaing to the default const values*/
     Witch();
+    /*overloads the applyEncounter function in Card and
+    implements a new version for it specifically when encountering 
+    Witch card*/
     void applyEncounter(Player& player) const override;
+    /*overloads the printCard function in Card and
+    implements a new version for it specifically when printing 
+    Witch card*/
     void printCard(std::ostream& os) const override;
+    /*cloning a Witch card*/
     BattleCard* clone() const override;
-
+    /*defining the d'tpr as default */
+    ~Witch() = default;
 };
 
 
