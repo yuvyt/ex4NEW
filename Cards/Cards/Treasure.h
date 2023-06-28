97694dd106e0c675e7cdf54ace19a454f3576858
yuvyt <130463664+yuvyt@users.noreply.github.com>
2023-06-28 17:12:40 +0300

    Update Treasure.h

--- Cards/Cards/Treasure.h
+++ Cards/Cards/Treasure.h
@@ -1,10 +1,10 @@
 #ifndef TREASURE_H
 #define TREASURE_H
 
-
-
 #include "Card.h"
 #include "../utilities.h"
+
+const int TREASURE_COINS = 10;
 
 class Treasure : public Card
 {
@@ -12,9 +12,16 @@ private:
     int m_treasure;
 
 public:
+    /* c'tor for Treasure, initializaing to the default const values*/
     Treasure();
+    /*overloads the applyEncounter function in Card and
+    implements a new version for it specifically when encountering 
+    Treasure card*/
     void applyEncounter(Player& player) const override;
+    /*cloning a Treasure card*/
     Card* clone() const override;
+    /*defining the d'tpr as default */
+    ~Treasure() = default;
 
 };
 
