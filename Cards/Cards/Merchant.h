2afd447a97c33e0eb6978e196efe202ed92ed2f0
yuvyt <130463664+yuvyt@users.noreply.github.com>
2023-06-28 16:45:57 +0300

    Update Merchant.h

--- Cards/Cards/Merchant.h
+++ Cards/Cards/Merchant.h
@@ -1,11 +1,15 @@
 #ifndef MERCHANT_H
 #define MERCHANT_H
 
-
-
-
 #include "Card.h"
 #include "../utilities.h"
+
+const int HEALTH_POINTS_TO_GIVE = 1;
+const int HEALTH_POINTS_PRICE = 5;  
+const int FORCE_POINTS_TO_GIVE = 1;
+const int FORCE_POINTS_PRICE = 10;
+
+
 
 class Merchant : public Card
 {
@@ -16,11 +20,20 @@ private:
     int m_forcePointsPrice;
 
 public:
+    /* c'tor for Merchant, initializaing to the default const values*/
     Merchant();
+    /*overloads the applyEncounter function in Card and
+    implements a new version for it specifically when encountering 
+    MERCHENT card*/
     void applyEncounter(Player& player) const override;
+    /*function to implement the process of buing a health point*/
     void applyMerchantHealthEncounter(Player& player) const;
+    /*function to implement the process of buing a force point*/
     void applyMerchantForceEncounter(Player& player) const;
+    /*cloning a Merchant card*/
     Card* clone() const override;
+    /*defining the d'tpr as default */
+    ~Merchant() = default;
 };
 
 #endif
