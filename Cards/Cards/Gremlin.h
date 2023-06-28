0a5de4f95b5910f1ff6417a7ee7c80489d3dafa0
yuvyt <130463664+yuvyt@users.noreply.github.com>
2023-06-28 17:57:57 +0300

    Update Gremlin.h

--- Cards/Cards/Gremlin.h
+++ Cards/Cards/Gremlin.h
@@ -3,7 +3,7 @@
 
 #include "BattleCard.h"
 
-const int HEALTH_POINTS_ON_LOSS = 10;
+const int HEALTH_POINTS_ON_LOSS = 10; 
 const int CARD_FORCE = 5;
 const int LOOT = 2;
 
@@ -14,11 +14,13 @@ public:
     Gremlin();
     /*overloads the applyEncounter function in Card and
     implements a new version for it specifically when encountering 
-    Gremlin card*/
+    Gremlin card
+    @param: player: player to apply the encounter on*/
     void applyEncounter(Player& player) const override;
     /*overloads the printCard function in Card and
     implements a new version for it specifically when printing 
-    Gremlin card*/
+    Gremlin card
+    @param: os: ostream to print to*/
     void printCard(std::ostream& os) const override;
     /*cloning a Gremlin card*/
     BattleCard* clone() const override;
