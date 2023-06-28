197ba5d663d38c2e1d6591a8bce6f34d26f95773
yuvyt <130463664+yuvyt@users.noreply.github.com>
2023-06-28 18:02:46 +0300

    Update Healer.h

--- Cards/Players/Healer.h
+++ Cards/Players/Healer.h
@@ -11,8 +11,14 @@ public:
     /*c'tor for Healer
     @param:playerName: the name of the player */
     Healer(const std::string& playerName);
+    /*overloding the heal function from player, so it
+    will heal the healer more
+    @param:HealthPointsToAdd: amount of original health point to add */
     void heal(int HealthPointsToAdd) override;
+    /*cloning the player*/
     Player* clone() const override;
+    /*setting the d'tpr to default*/
+    ~Healer() = default;
 };
 
 
